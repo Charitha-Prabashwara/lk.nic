56665aee92b30bc38a1dc6c65aabc2546a52a819
Charitha Prabhashwara <69114151+Charitha-Prabashwara@users.noreply.github.com>
2024-05-06 18:27:12 +0530

    create lknic class

---
+++ src/index.js
@@ -0,0 +1,45 @@
+/**
+    * Identifying the (1)type of ID card (old ID cards, new ID cards) after creating an
+    * Object using this class. of the ID card holder,
+    **    Type.
+    **    Birthday.
+    **    Age as of today.
+    **    Age on a certain date..
+    **    Gender.
+    * Can be output.
+    * @param  {String} nic  Identity card number of person. 
+    */
+class lknic{
+    #nic = null 
+    constructor(nic){
+
+    }
+
+    /**
+     * This returns true if the nic number is valid.
+     * @memberof lknic
+     * @name isValid
+     * @function
+     * @param {String} nic
+     * @returns {boolean}
+     */
+    static isValid(nic){
+        // function body   
+    }
+
+    /**
+     * This returns true if the nic number string length is valid.
+     * @memberof lknic
+     * @name lengthValdation
+     * @function static function
+     * @param {String} nic
+     * @returns {boolean}
+     */
+    static lengthValdation(nic){
+
+    }
+
+
+
+}
+

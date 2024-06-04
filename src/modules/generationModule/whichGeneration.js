b8231a1ea859de73d36fe3432851a90bac5dc2e9
Charitha Prabhashwara <69114151+Charitha-Prabashwara@users.noreply.github.com>
2024-06-05 00:27:00 +0530

    update docs strings

--- src/modules/generationModule/whichGeneration.js
+++ src/modules/generationModule/whichGeneration.js
@@ -2,35 +2,62 @@ import vxCheck from '../validationModule/v-and-xCheck.js';
 import lengthValidator from '../validationModule/lengthValidator.js';
 import ValidChars from '../validationModule/notInvalidChars.js';
 
-
+/**
+ * @module Generation
+ * @description Using "Generation", what is the generation of the Sri Lanka National ID number?
+ * Can be identified. Generation can only be identified in valid ID numbers.
+ * @author Charitha Prabhashwara
+ * @email prabhashwara.seu@gmail.com
+ * @date 2024/06/03
+ */
 class Generation{
-    #isLengthValid(nic){return lengthValidator(nic);}
-    #isValidChar(nic){return ValidChars(nic)} 
-    #vxCheck(nic){return new vxCheck(nic).isValid();} 
 
-    #determiningTheGenerationFromChar(nic){
+    #isLengthValid(nationalIdentityCardNumber){return lengthValidator(nationalIdentityCardNumber);}
+    #isValidChar(nationalIdentityCardNumber){return ValidChars(nationalIdentityCardNumber)} 
+    #vxCheck(nationalIdentityCardNumber){return new vxCheck(nationalIdentityCardNumber).isValid();} 
+
+    /**
+     * @method determiningTheGenerationFromChar
+     * @description When coming from first-generation IDs to second-generation,
+     * the last letter "V/X" used to determine whether the first-generation ID
+     * is eligible to vote or not can be used to identify the generation.
+     * Generation 1 as "1" and Generation 2 as "2". Also returns "false" if the validation test fails.
+     * @param {String} nationalIdentityCardNumber
+     * @returns {false | "1" | "2"}
+     * @date 2024/06/03
+     */
+    #determiningTheGenerationFromChar(nationalIdentityCardNumber){
         
-        if(!this.#isLengthValid(nic)){return false;}
-        if(!this.#isValidChar(nic)){return false;}
+        if(!this.#isLengthValid(nationalIdentityCardNumber)){return false;}
+        if(!this.#isValidChar(nationalIdentityCardNumber)){return false;}
 
-        if(this.#vxCheck(nic)){
-            const isOldGen = new vxCheck(nic).isOldGeneration();
-            const isNewGen = new vxCheck(nic).isNewGeneration();
+        if(this.#vxCheck(nationalIdentityCardNumber)){
+            const isOldGen = new vxCheck(nationalIdentityCardNumber).isOldGeneration();
+            const isNewGen = new vxCheck(nationalIdentityCardNumber).isNewGeneration();
             
             if(isOldGen){return "1";}
             if(isNewGen){return "2";}
-        }else{return false;}
+        }
+        return false;
     }
+    /**
+     * @method determiningTheGenerationFromLength
+     * @description The increase in the number of characters in the ID number from the first
+     * generation to the second generation is used to identify the generation.
+     * Generation 1 as "1" and Generation 2 as "2". Also returns "false" if the validation test fails.
+     * @param {String} nationalIdentityCardNumber
+     * @returns {false | "1" | "2"}
+     * @date 2024/06/03
+     */
+    #determiningTheGenerationFromLength(nationalIdentityCardNumber){
 
-    #determiningTheGenerationFromLength(nic){
+        if(!this.#isLengthValid(nationalIdentityCardNumber)){return false;}
+        if(!this.#isValidChar(nationalIdentityCardNumber)){return false;}
 
-        if(!this.#isLengthValid(nic)){return false;}
-        if(!this.#isValidChar(nic)){return false;}
-
-        if(this.#vxCheck(nic)){
+        if(this.#vxCheck(nationalIdentityCardNumber)){
             
-           const oldGen = nic.length == 10;
-           const newGen = nic.length == 12;
+           const oldGen = nationalIdentityCardNumber.length == 10;
+           const newGen = nationalIdentityCardNumber.length == 12;
 
            if(oldGen && !newGen){
                 return "1";
@@ -39,13 +66,24 @@ class Generation{
             }else{
                 return false;
             }
-        }else{return false;}
+        }
+        return false;
 
     }
 
-    witchGeneration(nic){
-        const gfc =  this.#determiningTheGenerationFromChar(nic);
-        const gfcl = this.#determiningTheGenerationFromLength(nic);
+    /**
+     * @method witchGeneration
+     * @description From the first generation to the second generation,
+     * the removal of the characters "V/X" and the increase in the number
+     * of characters are both used to determine the generation of the National ID number.
+     * Generation 1 as "1" and Generation 2 as "2". Also returns "false" if the validation test fails.
+     * @param {String} nationalIdentityCardNumber
+     * @returns {false | "1" | "2"}
+     * @date 2024/06/03
+     */
+    witchGeneration(nationalIdentityCardNumber){
+        const gfc =  this.#determiningTheGenerationFromChar(nationalIdentityCardNumber);
+        const gfcl = this.#determiningTheGenerationFromLength(nationalIdentityCardNumber);
         
         if((gfc == false) || (gfcl == false)){
             return false;
@@ -55,9 +93,8 @@ class Generation{
             return '1';
         }else if((gfc == '2') && (gfcl == '2')){
             return '2';
-        }else{
-            return false;
         }
+        return false;
     }
 }
 

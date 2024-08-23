a37980c07b1ece9c33068e6bc4da80d5a5a5dbbd
Charitha Prabhashwara <69114151+Charitha-Prabashwara@users.noreply.github.com>
2024-08-23 11:13:04 +0530

    Create documentation - JsDoc

--- src/exceptions/exceptions.js
+++
@@ -1,7 +0,0 @@
-import InvalidLengthException from './InvalidLengthException';
-import invalidCharacterException from './InvalidCharacterException';
-import vxNotFoundInOldGeneration from './vxNotFoundInOldGeneration'
-
-export const InvalidLengthException = new InvalidLengthException();
-export const invalidCharacterException = new invalidCharacterException();
-export const vxNotFoundInOldGeneration = new vxNotFoundInOldGeneration();
